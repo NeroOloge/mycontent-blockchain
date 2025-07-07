6c2d2dcb4e4b95cd033066eaf181083580bc67d8
Oghenero Ologe <nologe37@gmail.com>
2025-07-07 12:04:19 +0100

    refactored functions to accept timestamp

--- contracts/DecentralisedBlog.sol
+++ contracts/DecentralisedBlog.sol
@@ -3,19 +3,19 @@
 pragma solidity >=0.8.2 < 0.9.0;
 
 contract DecentralisedBlog {
-  event PostCreated(uint256 indexed postId, address indexed author, string cid, string title);
+  event PostCreated(uint256 indexed postId, address indexed author, string cid, string title, uint256 indexed timestamp);
   event PostDeleted(uint256 indexed postId);
-  event TagAdded(uint256 indexed postId, string tag);
-  event CommentAdded(uint256 indexed commentId, uint256 indexed postId, address commenter, string cid);
+  event TagAdded(uint256 indexed postId, string tag, uint256 indexed timestamp);
+  event CommentAdded(uint256 indexed commentId, uint256 indexed postId, address commenter, string cid, uint256 indexed timestamp);
   event CommentDeleted(uint256 indexed commentId);
   event PostLiked(uint256 indexed postId, address indexed liker);
   event PostUnliked(uint256 indexed postId, address indexed unliker);
   event PostBookmarked(uint256 indexed postId, address indexed bookmarker);
   event PostUnbookmarked(uint256 indexed postId, address indexed unbookmarker);
-  event Followed(address indexed follower, address indexed following);
+  event Followed(address indexed follower, address indexed following, uint256 indexed timestamp);
   event Unfollowed(address indexed follower, address indexed following);
-  event Tipped(address indexed from, address indexed to, uint256 amount);
-  event ProfileUpdated(address indexed user, string username, string bio, string imageCID);
+  event Tipped(address indexed from, address indexed to, uint256 amount, uint256 indexed timestamp);
+  event ProfileUpdated(address indexed user, string username, string bio, string imageCID, uint256 indexed timestamp);
 
   struct Post {
     uint256 id;
@@ -72,12 +72,12 @@ contract DecentralisedBlog {
   }
 
   function createPost(string calldata cid, string[] calldata tags, 
-    string[] calldata imageCIDs, string calldata title) external {
+    string[] calldata imageCIDs, string calldata title, uint256 timestamp) external {
     posts[nextPostId] = Post({
       id: nextPostId,
       author: msg.sender,
       cid: cid,
-      timestamp: block.timestamp,
+      timestamp: timestamp,
       likes: 0,
       bookmarks: 0,
       imageCIDs: imageCIDs,
@@ -88,9 +88,9 @@ contract DecentralisedBlog {
     userPosts[msg.sender].push(nextPostId);
     reputation[msg.sender] += 10;
 
-    emit PostCreated(nextPostId, msg.sender, cid, title);
+    emit PostCreated(nextPostId, msg.sender, cid, title, timestamp);
     for (uint i = 0; i < tags.length; i++) {
-      emit TagAdded(nextPostId, tags[i]);
+      emit TagAdded(nextPostId, tags[i], timestamp);
     }
     nextPostId++;
   }
@@ -102,7 +102,7 @@ contract DecentralisedBlog {
     emit PostDeleted(postId);
   }
 
-  function addComment(uint256 postId, string calldata cid) external {
+  function addComment(uint256 postId, string calldata cid, uint256 timestamp) external {
     require(posts[postId].exists, "Post doesn't exist");
 
     comments[nextCommentId] = Comment({
@@ -110,14 +110,14 @@ contract DecentralisedBlog {
       postId: postId, 
       commenter: msg.sender, 
       cid: cid, 
-      timestamp: block.timestamp,
+      timestamp: timestamp,
       isDeleted: false,
       exists: true
     });
     postComments[postId].push(nextCommentId);
 
     reputation[msg.sender] += 3;
-    emit CommentAdded(nextCommentId, postId, msg.sender, cid);
+    emit CommentAdded(nextCommentId, postId, msg.sender, cid, timestamp);
     nextCommentId++;
   }
 
@@ -172,14 +172,14 @@ contract DecentralisedBlog {
     emit PostUnbookmarked(postId, msg.sender);
   }
 
-  function follow(address userToFollow) external {
+  function follow(address userToFollow, uint256 timestamp) external {
     require(userToFollow != msg.sender, "Can't follow yourself!");
     require(!isFollowing[msg.sender][userToFollow], "Already following!");
 
     isFollowing[msg.sender][userToFollow] = true;
     followerCount[userToFollow]++;
     followingCount[msg.sender]++;
-    emit Followed(msg.sender, userToFollow);
+    emit Followed(msg.sender, userToFollow, timestamp);
   }
 
   function unfollow(address userToUnfollow) external {
@@ -191,7 +191,7 @@ contract DecentralisedBlog {
     emit Unfollowed(msg.sender, userToUnfollow);
   }
 
-  function tipAuthor(uint256 postId) external payable {
+  function tipAuthor(uint256 postId, uint256 timestamp) external payable {
     address author = posts[postId].author;
     require(author != address(0), "Invalid author");
     require(msg.value > 0, "No tip sent");
@@ -199,10 +199,11 @@ contract DecentralisedBlog {
     (bool success, ) = author.call{value: msg.value}("");
     require(success, "Transfer failed");
 
-    emit Tipped(msg.sender, author, msg.value);
+    emit Tipped(msg.sender, author, msg.value, timestamp);
   }
 
-  function updateProfile(string calldata username, string calldata bio, string calldata imageCID) 
+  function updateProfile(string calldata username, string calldata bio, 
+    string calldata imageCID, uint256 timestamp) 
   external {
     profiles[msg.sender] = UserProfile({
       username: username,
@@ -210,7 +211,7 @@ contract DecentralisedBlog {
       imageCID: imageCID,
       exists: true
     });
-    emit ProfileUpdated(msg.sender, username, bio, imageCID);
+    emit ProfileUpdated(msg.sender, username, bio, imageCID, timestamp);
   }
 
   function getPostsByAuthor(address author) external view returns (uint256[] memory) {
--- ignition/modules/Blogging.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const BloggingModule = buildModule("BloggingModule", (m) => {
-  const blogging = m.contract("Blogging", [])
-
-  return { blogging }
-})
-
-export default BloggingModule
